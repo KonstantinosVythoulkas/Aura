a61d46d6a230e9954dbf0ade6f44853035245b58
Kalista <kostas.vythoulkasuk@gmail.com>
2024-12-10 18:00:58 +0000

    Interaction interface

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,3 +3,12 @@
 
 #include "Character/AuraEnemy.h"
 
+void AAuraEnemy::HighLightActor()
+{
+	UE_LOG(LogTemp, Warning, TEXT("HIGHLIGHT_ACTOR"));
+}
+
+void AAuraEnemy::UnHighLightActor()
+{
+	UE_LOG(LogTemp, Warning, TEXT("UNHIGHLIGHT_ACTOR"));
+}
