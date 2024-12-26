8c6ecb7e7cfb8cb8b8a37791ce32310f0c35639f
Kalista <kostas.vythoulkasuk@gmail.com>
2024-12-26 07:58:33 +0000

    Singleton tags setup

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// VKGames
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attribute.Secondary.Armor"), FString("Reduce damage taken, improves Block chance"));
+}
